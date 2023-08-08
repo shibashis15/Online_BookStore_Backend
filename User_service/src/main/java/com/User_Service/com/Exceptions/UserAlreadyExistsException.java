15ffb909ec257c7d90394cc52c25890bb12e1322
Shibashis Banerjee <69764940+shibashis15@users.noreply.github.com>
2023-08-09 03:47:22 +0530

    Delete User_service directory

--- User_service/src/main/java/com/User_Service/com/Exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.User_Service.com.Exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- User_service/src/main/java/com/User_Service/com/Exceptions/UserNotPresent.java
+++
@@ -1,7 +0,0 @@
-package com.User_Service.com.Exceptions;
-
-public class UserNotPresent extends RuntimeException {
-    public UserNotPresent(String message) {
-        super(message);
-    }
-}
